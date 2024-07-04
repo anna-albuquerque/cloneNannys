83adff98af7691640dc2cc5b556f7149df32cd37
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2024-07-04 11:11:00 -0300

    Update urls.py

--- djangoRestApp/nanny_pets/nanny_pets_app/urls.py
+++ djangoRestApp/nanny_pets/nanny_pets_app/urls.py
@@ -7,9 +7,9 @@ from .views import CaracteristicasAPIView,CaracteristicasDoCuidadorView, Cuidado
 
 
 urlpatterns = [
-    path('cuidadores/', CuidadorAPIView.as_view(), name='cuidadores'),
-    path('caracteristicas/', CaracteristicasAPIView.as_view(), name='caracteristicas'),
-    path('tutores/',TutorAPIView.as_view(), name='tutores'),
-    path('cuidadores/<int:cuidador_id>/caracteristicas/',CaracteristicasDoCuidadorView.as_view(), name='caracteristicascuidador'),
-    path('cuidadores-filtrados/', CuidadorFiltradoView.as_view(), name='cuidadores-filtrados'),
+    path('https://annacarolinneam.pythonanywhere.com/api/v1/cuidadores/', CuidadorAPIView.as_view(), name='cuidadores'),
+    path('https://annacarolinneam.pythonanywhere.com/api/v1/caracteristicas/', CaracteristicasAPIView.as_view(), name='caracteristicas'),
+    path('https://annacarolinneam.pythonanywhere.com/api/v1/tutores/',TutorAPIView.as_view(), name='tutores'),
+    path('https://annacarolinneam.pythonanywhere.com/api/v1/cuidadores/<int:cuidador_id>/caracteristicas/',CaracteristicasDoCuidadorView.as_view(), name='caracteristicascuidador'),
+    path('https://annacarolinneam.pythonanywhere.com/api/v1/cuidadores-filtrados/', CuidadorFiltradoView.as_view(), name='cuidadores-filtrados'),
 ]
